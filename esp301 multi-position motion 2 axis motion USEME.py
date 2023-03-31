6669fda8de759b8e2521cf32528cee9ebdc2c785
ryandouglasa <115324187+ryandouglasa@users.noreply.github.com>
2023-03-31 11:12:10 -0700

    Update esp301 multi-position motion 2 axis
    Now sets velocity of both axes to 10 mm/s.

--- esp301 multi-position motion 2 axis motion USEME.py
+++ esp301 multi-position motion 2 axis motion USEME.py
@@ -1,8 +1,8 @@
 import espModeStirring
 import time
-controller = espModeStirring.espModeStirring("COM3",921600, 1, False, 300.0, [1,2]) # newport controller device, baud rate, default axis, reset, initial position, axes in use
+controller = espModeStirring.espModeStirring("COM3",921600, 1, True, 50.0, [1,2]) # newport controller device, baud rate, default axis, reset, initial position, axes in use
 
-#takes an array of any length where each element is a 3-element array in the format [x, y, waitTime]
+#takes an array of any length where each element is a 3-element array in the format [x, y, waitTime]  
 #and makes the controller visit each coordinate and wait for the time given (seconds)
 def multiMotion(array):
     for subArray in array:
@@ -12,17 +12,24 @@ def multiMotion(array):
 
 #testbench
 coordinates = [
-    [0, 0, 1],
-    [550, 550, 2],
+    [80, 50, 7],
+    [110, 50, 7],
+    [140, 50, 10],
+    [140, 80, 7],
+    [140, 110, 7],
+    [140, 140, 7]
 ]
 
-controller.empty_errors()
+#controller.empty_errors()
 
-controller.debug()
+controller.setvel(10,1)
+controller.setvel(10,2)
+
+#controller.debug()
 multiMotion(coordinates)
-controller.debug()
+#controller.debug()
 
-controller.empty_errors()
+#controller.empty_errors()
 
 #resets position
-controller.setpos2(300,300,1,2)
+controller.setpos2(0,300,1,2)
