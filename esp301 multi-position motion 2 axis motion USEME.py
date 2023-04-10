af226aaececa253d466b71c580212d57f85748e4
ryandouglasa <115324187+ryandouglasa@users.noreply.github.com>
2023-04-10 15:06:57 -0700

    Update esp301 multi-position motion 2 axis motion USEME.py
    updated with use of .TX() function to allow for better synchronization

--- esp301 multi-position motion 2 axis motion USEME.py
+++ esp301 multi-position motion 2 axis motion USEME.py
@@ -1,35 +1,41 @@
 import espModeStirring
 import time
-controller = espModeStirring.espModeStirring("COM3",921600, 1, True, 50.0, [1,2]) # newport controller device, baud rate, default axis, reset, initial position, axes in use
+controller = espModeStirring.espModeStirring("COM8",921600, 1, True, 50.0, [1,2]) # newport controller device, baud rate, default axis, reset, initial position, axes in use
 
 #takes an array of any length where each element is a 3-element array in the format [x, y, waitTime]  
 #and makes the controller visit each coordinate and wait for the time given (seconds)
 def multiMotion(array):
     for subArray in array:
-        controller.setpos2wait(subArray[0],subArray[1], 1, 2)
+        controller.setpos2(subArray[0],subArray[1], 1, 2)
+        waiting = controller.TX()
+        while (waiting != (b'@\r\n')):
+            time.sleep(0.5)
+            waiting = controller.TX()
         print("now waiting for %d seconds"%(subArray[2]))
         time.sleep(subArray[2])
 
 #testbench
 coordinates = [
-    [80, 50, 7],
-    [110, 50, 7],
-    [140, 50, 10],
-    [140, 80, 7],
-    [140, 110, 7],
-    [140, 140, 7]
+    [50, 0, 2],
+    [55, 0, 2],
+    [60, 0, 2],
+    [65, 0, 2],
+    [70, 0, 2],
+    [75, 0, 2],
+    [80, 0, 2],
+    [85, 0, 2]
 ]
 
 #controller.empty_errors()
 
-controller.setvel(10,1)
-controller.setvel(10,2)
+#controller.setvel(10,1)
+#controller.setvel(10,2)
 
-#controller.debug()
+controller.debug()
 multiMotion(coordinates)
-#controller.debug()
+controller.debug()
 
-#controller.empty_errors()
+controller.empty_errors()
 
 #resets position
-controller.setpos2(0,300,1,2)
+controller.setpos2(50,0,1,2)
